c039a8f5b4838bf3a0cfb4bd45f046a0df4cb522
Ian Jones <ian@cobi.co.uk>
2021-06-14 13:31:51 +0100

    Add setup files

---
+++ readme.md
@@ -0,0 +1,7 @@
+Django app to render menus and load tabs with Ajax
+
+See example django project with docker compose file 
+
+Add to installed apps in settings   
+`'django_menus',`
+    
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="django-tab-menus",
+    version="0.0.1",
+    author="Ian Jones",
+    description="Django app to render menus and load tabs with Ajax",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jonesim/django-menus",
+    include_package_data = True,
+    packages=['django_menus'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
