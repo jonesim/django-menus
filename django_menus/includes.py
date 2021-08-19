2e2707631e426132a0ac699fdab920a3bc1a002e
jonesim <ian@cobi.co.uk>
2021-08-19 12:51:57 +0100

    Add js and css includes.py

---
+++ django_menus/includes.py
@@ -0,0 +1,14 @@
+from ajax_helpers.html_include import SourceBase, pip_version
+
+version = pip_version('django-tab-menus')
+
+
+class DjangoMenus(SourceBase):
+    static_path = 'django_menus/'
+    filename = 'django_menus'
+    js_path = ''
+    css_path = ''
+
+
+class DefaultInclude(DjangoMenus):
+    pass
