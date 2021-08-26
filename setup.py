588ea5ea220f537418d22abcf1a91d71bb2248be
jonesim <ian@cobi.co.uk>
2021-08-26 18:36:20 +0100

    Up version to 0.0.13

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r") as fh:
 
 setuptools.setup(
     name="django-tab-menus",
-    version="0.0.12",
+    version="0.0.13",
     author="Ian Jones",
     description="Django app to render menus and load tabs with Ajax",
     long_description=long_description,
