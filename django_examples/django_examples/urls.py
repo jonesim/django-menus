b52458217472b617172ed8ca12084c181c7ce953
jonesim <ian@cobi.co.uk>
2021-11-11 10:49:47 +0000

    Add favicon

--- django_examples/django_examples/urls.py
+++ django_examples/django_examples/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('', include('show_src_code.urls')),
     path('admin/', admin.site.urls),
+    path('favicon.ico', RedirectView.as_view(url='/static/menu_examples/favicon.ico', permanent=True)),
 ]
