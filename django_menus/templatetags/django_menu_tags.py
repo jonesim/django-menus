5f4ef780e754d14f718b7f59698ed54224485ba5
ian_jones <ian@cobi.co.uk>
2026-08-06 16:35:52 +0100

    Quote the id attribute in menu_content and template_content
    Unquoted id= broke whenever the content name contained a character that
    ends an unquoted HTML attribute.

--- django_menus/templatetags/django_menu_tags.py
+++ django_menus/templatetags/django_menu_tags.py
@@ -8,12 +8,12 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def template_content(context, content_name):
-    return mark_safe(f'<div id={content_name}>{context[content_name]}</div>')
+    return mark_safe(f'<div id="{content_name}">{context[content_name]}</div>')
 
 
 @register.simple_tag(takes_context=True)
 def menu_content(context, content_name):
-    return mark_safe(f'<div id={content_name}>{context["menus"][content_name].render()}</div>')
+    return mark_safe(f'<div id="{content_name}">{context["menus"][content_name].render()}</div>')
 
 
 @register.simple_tag()
